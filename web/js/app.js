5f7ea12a477592dc4e4f82e77036f35a5d5b7378
yuyuyzl <yuzhongliang22@163.COM>
2018-05-29 15:45:31 +0800

    IOS主题，开始画界面

--- web/js/app.js
+++ web/js/app.js
@@ -6,7 +6,7 @@ var app  = new Framework7({
   root: '#app', // App root element
   id: 'io.framework7.testapp', // App bundle ID
   name: 'Framework7', // App name
-  theme: 'auto', // Automatic theme detection
+  theme: 'ios', // Automatic theme detection
   // App root data
   data: function () {
     return {
